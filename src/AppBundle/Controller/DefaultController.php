dbdb8e2a2daa94cf94759604e65fe50ff6a6a6ee
lyrad <remi@valentie.fr>
2016-06-02 03:09:48 -0700

    Basic form okay, templating tweets ok, session query terms ok

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,10 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use AppBundle\Entity\TwitterSearch;
+use AppBundle\Form\Type\TwitterSearchType;
+
 
 class DefaultController extends Controller
 {
@@ -13,6 +17,58 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+	// Start session
+	// $session = new Session();
+	// $session->start();
+	$session = $request->getSession();
+
+	if( false === $session->has('previous_q') ) {
+		$previous_q = array();
+		$session->set('previous_q', $previous_q);
+	}
+
+	// Create TwitterSearch object
+	$twittersearch = new TwitterSearch();
+
+	// Create form
+	$form = $this->createForm(TwitterSearchType::class, $twittersearch);
+
+	 $form->handleRequest($request);
+
+    	if($form->isSubmitted() && $form->isValid()) {
+		// Save q in session
+		$previous_q = $session->get('previous_q');
+		$previous_q[] = $form->get('q')->getData();
+		$session->set('previous_q', $previous_q);
+
+		$endroidtwitter = $this->get('endroid.twitter');
+
+		$response = $endroidtwitter->query('search/tweets', 'GET', 'json', array(
+			'q' => $form->get('q')->getData(),
+			'count' => $form->get('count')->getData()
+		));
+		
+		$tweets = json_decode($response->getContent());
+
+    	} else {
+		$tweets = [];
+	}
+
+	return $this->render(
+                'default/index.html.twig',
+                array( 
+			'form' => $form->createView(),
+			'tweets' => $tweets,
+			'previous_q' => $previous_q
+		)
+        );	
+
+
+
+
+
+
+
 	$twitter = $this->get('endroid.twitter');
 
 	// Retrieve the user's timeline
